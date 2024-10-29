99f7efb3c87e54e987eb2d0e87f12644eeeaf6e2
hisham.hsbr <hisham.hsbr@gmail.com>
2024-10-29 02:50:04 -0400

    language setup & frontend started

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,31 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
+
+// language change
+Route::get('/change-locale/{locale}', function ($locale) {
+    if (in_array($locale, haystack: ['en', 'es', 'ml'])) {
+        session(['locale' => $locale]);
+        App::setLocale($locale);
+    }
+    return redirect()->back();
+})->name('change-locale');
+
+
+
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
+
+
+
+
+require __DIR__ . '/auth.php';
+require __DIR__ . '/frontend.php';
+require __DIR__ . '/backend.php';
