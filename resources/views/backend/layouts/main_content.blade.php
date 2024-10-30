481a9fb278c0f87fb032706ede50b644ed6e6f9c
hisham-hsbr <hisham.hsbr@gmail.com>
2024-10-30 09:12:55 -0400

    some updates in admin dashboard

---
+++ resources/views/backend/layouts/main_content.blade.php
@@ -0,0 +1,18 @@
+<!-- start page title -->
+<div class="py-3 py-lg-4">
+    <div class="row">
+        <div class="col-lg-6">
+            <h4 class="mb-0 page-title">Starter</h4>
+        </div>
+        <div class="col-lg-6">
+            <div class="d-none d-lg-block">
+                <ol class="m-0 breadcrumb float-end">
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">Extra Pages</a>
+                    </li>
+                    <li class="breadcrumb-item active">Starter</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- end page title -->
